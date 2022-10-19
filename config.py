671aad4fede9a886da949c1409bdbe91becdeeb0
Paul Nepomuceno <sk8butt420@gmail.com>
2022-10-19 18:20:20 +0800

    removed user logins

--- config.py
+++ config.py
@@ -4,16 +4,7 @@
 MAX_DELAY = 2.5
 
 #PROXIES
-proxies = {"user_1": {"proxy_port": "185.158.104.152:12345:tbtbtb:mxVFyaOE", "username" : "cqvekkqhohoojj@mail.ru", "password" : "cqvekkqhohoojj",},
-            "user_2": {"proxy_port" : "196.245.187.203:12345:tbtbtb:mxVFyaOE","username" : "qkspfkpywrlnnm@mail.ru","password" : "qkspfkpywrlnnm",},
-            "user_3": {"proxy_port" : "165.231.133.236:12345:tbtbtb:mxVFyaOE", "username" : "xushrqbtd@mail.ru", "password" : "xushrqbtd",},
-            "user_4": {"proxy_port" : "23.231.15.178:12345:tbtbtb:mxVFyaOE", "username" : "yrpdofwlou@mail.ru", "password" : "yrpdofwlou",},
-            "user_5": {"proxy_port" : "50.3.194.250:12345:tbtbtb:mxVFyaOE", "username" : "dusmncylkobgz@mail.ru", "password" : "dusmncylkobgz",},
-            "user_6": {"proxy_port" : "5.157.23.246:12345:tbtbtb:mxVFyaOE", "username" : "zilljlqiaul@mail.ru", "password" : "zilljlqiaul",},
-            "user_7": {"proxy_port" : "23.231.14.130:12345:tbtbtb:mxVFyaOE", "username" : "jfvdjfpqzhfzrac@mail.ru", "password" : "jfvdjfpqzhfzrac",},
-            "user_8": {"proxy_port" : "50.3.193.195:12345:tbtbtb:mxVFyaOE", "username" : "trofim.bakay.bakaykin@mail.ru", "password" : "trofim.bakay.bakaykin",},
-            "user_9": {"proxy_port" : "5.157.23.133:12345:tbtbtb:mxVFyaOE", "username" : "ksyu.solynskaya@mail.ru", "password" : "ksyu.solynskaya",},  
-            "user_10": {"proxy_port" : "45.95.116.133:12345:tbtbtb:mxVFyaOE", "username" : "theodore.miers.79@inbox.ru", "password" : "theodore.miers.79",}, 
+proxies = {"user" : {"proxy_port": "sample", "username":"sample", "password": "sample"}
             }
 
 
